4dc752243e95ed334b59d2a903a584b04c95f3ba
francesco_pacifico <francescopacifico1905@gmail.com>
2023-06-21 21:05:43 +0200

    aggiunti algoritmi: quick sort, merge sort

--- src/main/java/it/francesco/sorting/stuff/SortingAlgorithm.java
+++ src/main/java/it/francesco/sorting/stuff/SortingAlgorithm.java
@@ -1,5 +1,12 @@
 package it.francesco.sorting.stuff;
 
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.function.Consumer;
 
 public enum SortingAlgorithm {
@@ -45,7 +52,98 @@ public enum SortingAlgorithm {
             }
         }
 
-    });
+    }),
+
+    QUICK_SORT("QUICK SORT", null) {
+
+        @Override
+        public Consumer<BlockList> getAlgorithm() {
+            return blockList -> {
+                quickSort(blockList, 0, blockList.length() - 1);
+            };
+        }
+
+        private void quickSort(BlockList blockList, int start, int end) {
+            if(end - start >= 1) {
+                int pivotIndex = new Random().nextInt(start, end + 1);
+                pivotIndex = partition(blockList, start, end, pivotIndex);
+                quickSort(blockList, start, pivotIndex - 1);
+                quickSort(blockList, pivotIndex + 1, end);
+            }
+        }
+
+        private int partition(BlockList blockList, int start, int end, int pivotIndex) {
+            double pivot = blockList.get(pivotIndex).getHeight();
+            blockList.swapBlocks(pivotIndex, end);
+            int i = start;
+            int j = end - 1;
+            while(i < j) {
+                if(blockList.get(i).getHeight() >= pivot) {
+                    blockList.swapBlocks(i, j--);
+                    blockList.pause(20);
+                }
+                else {
+                    i++;
+                }
+            }
+            blockList.pause(20);
+            if(blockList.get(i).getHeight() >= pivot) {
+                blockList.swapBlocks(i, end);
+                return i;
+            }
+            else {
+                blockList.swapBlocks(i + 1, end);
+                return i + 1;
+            }
+        }
+
+    },
+
+    MERGE_SORT("MERGE SORT", null) {
+
+        @Override
+        public Consumer<BlockList> getAlgorithm() {
+            return blockList -> {
+                mergeSort(blockList, 0, blockList.length() - 1);
+            };
+        }
+
+        private void mergeSort(BlockList blockList, int start, int end) {
+            if(end - start >= 1) {
+                int mid = (end + start) / 2;
+                mergeSort(blockList, start, mid);
+                mergeSort(blockList, mid + 1, end);
+                merge(blockList, start, end, mid);
+            }
+        }
+
+        private void merge(BlockList blockList, int start, int end, int mid) {
+            int i = start;
+            int j = mid + 1;
+            int k = 0;
+            List<Pair<Double, Paint>> temp = new ArrayList<>(end - start + 1);
+            while(i <= mid && j <= end) {
+                if(blockList.get(i).getHeight() < blockList.get(j).getHeight()) {
+                    temp.add(k++, new Pair<>(blockList.get(i).getHeight(), blockList.get(i++).getFill()));
+                }
+                else {
+                    temp.add(k++, new Pair<>(blockList.get(j).getHeight(), blockList.get(j++).getFill()));
+                }
+            }
+            while(i <= mid) {
+                temp.add(k++, new Pair<>(blockList.get(i).getHeight(), blockList.get(i++).getFill()));
+            }
+            while(j <= end) {
+                temp.add(k++, new Pair<>(blockList.get(j).getHeight(), blockList.get(j++).getFill()));
+            }
+            for(k = 0; k < temp.size(); k++) {
+                blockList.get(start + k).setHeight(temp.get(k).getKey());
+                blockList.get(start + k).setFill(temp.get(k).getValue());
+                blockList.pause(20);
+            }
+        }
+
+    };
 
     private final String name;
     private final Consumer<BlockList> algorithm;
